a53cc993dd35d24f36338d8e8fdb0298f3721ac6
landerf <lukas.paquay@outlook.be>
2022-11-28 16:52:11 +0100

    🚀 Add cond in message create

--- src/events/discord/message/messageCreate.js
+++ src/events/discord/message/messageCreate.js
@@ -1,6 +1,5 @@
 const { Message } = require("discord.js");
 const chalk = require("chalk");
-const Guild = require(`../../../schemas/guild`);
 const { sendLogMessage } = require("../../../functions/tools/sendLogMessage");
 module.exports = {
   name: "messageCreate",
@@ -9,18 +8,24 @@ module.exports = {
    * @param {Client} client
    */
   async execute(message, client) {
-    try {
-      await sendLogMessage(
-        {
-          author: message.member.user,
-          guild: message.guild,
-          content: message.content,
-        },
-        `Message create log`,
-        client
+    if (message.author.bot) {
+      console.warn(
+        `[Event] messageCreate : ${chalk.red("from another bot, do anything.")}`
       );
-    } catch (e) {
-      console.error(e);
+    } else {
+      try {
+        await sendLogMessage(
+          {
+            author: message.member.user,
+            guild: message.guild,
+            content: message.content,
+          },
+          `Message create log`,
+          client
+        );
+      } catch (e) {
+        console.error(e);
+      }
     }
   },
 };
